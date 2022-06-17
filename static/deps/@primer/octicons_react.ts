52eb14dfd44538171b86e87ebe531ba006e95b07
undefined <sjx233@qq.com>
2022-06-17 19:58:27 +0800

    update deps

--- static/deps/@primer/octicons_react.ts
+++ static/deps/@primer/octicons_react.ts
@@ -1,1 +1,1 @@
-export * from "https://esm.sh/@primer/octicons-react@17.2.0?deps=react@18.1.0&target=es2020&pin=v86";
+export * from "https://esm.sh/@primer/octicons-react@17.2.0?deps=react@18.2.0&target=es2020&pin=v86";
--- static/deps/react_router_dom.ts
+++ static/deps/react_router_dom.ts
@@ -1,1 +1,1 @@
-export * from "https://esm.sh/react-router-dom@6.3.0?deps=react@18.1.0&target=es2020&pin=v86";
+export * from "https://esm.sh/react-router-dom@6.3.0?deps=react@18.2.0&target=es2020&pin=v86";
