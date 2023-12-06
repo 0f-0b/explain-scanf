ca4ef5a635d92e675977f53b3db8ba3dd5d073f5
ud2 <sjx233@qq.com>
2023-12-07 07:58:46 +0800

    `HttpServer.shutdown` is stabilized

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-#!/usr/bin/env -S deno run --unstable-http --unstable-kv --allow-read=dist,index.html,robots.txt --allow-net=0.0.0.0
+#!/usr/bin/env -S deno run --unstable-kv --allow-read=dist,index.html,robots.txt --allow-net=0.0.0.0
 
 import { fail } from "./fail.ts";
 import { onError, toStdHandler } from "./handler.ts";
